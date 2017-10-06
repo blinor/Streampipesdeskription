792d9cfffb58a35cf4eddef13eef25799d286da0
blinority <blinority@gmail.com>
2017-10-06 10:11:55 +0200

    added Javadoc

--- src/main/java/ba/weather/WeatherSourcesInit.java
+++ src/main/java/ba/weather/WeatherSourcesInit.java
@@ -3,26 +3,19 @@ package ba.weather;
 import org.streampipes.container.init.DeclarersSingleton;
 import org.streampipes.container.standalone.init.StandaloneModelSubmitter;
 
-/*
-    TUTORIAL:
-    This is the main class of the examples sources project.
-    Here you must register all the event producers. A producer is a collection of different streams.
-    The main method starts a web server that provides all the descriptions of the registered
-    producers and their streams.
+/**
+ * @author Michael Jahns
+ * this is build on the template provided by StreamPipes
+ * It will provide the data to add the source to StreamPipes
  */
 public class WeatherSourcesInit extends StandaloneModelSubmitter {
 
-    public static void main(String[] args) {
+	public static void main(String[] args) {
 
-        /*
-            TOTORIAL:
-            Add the newly created event producer to the declarer singleton
-         */
-        DeclarersSingleton.getInstance()
-                .add(new WeatherSource());
-        DeclarersSingleton.getInstance().setHostName(WeatherConfig.INSTANCE.getHost());
-        DeclarersSingleton.getInstance().setPort(WeatherConfig.INSTANCE.getPort());
-        new WeatherSourcesInit().init();
-    }
+		DeclarersSingleton.getInstance().add(new WeatherSource());
+		DeclarersSingleton.getInstance().setHostName(WeatherConfig.INSTANCE.getHost());
+		DeclarersSingleton.getInstance().setPort(WeatherConfig.INSTANCE.getPort());
+		new WeatherSourcesInit().init();
+	}
 
 }
--- src/main/java/ba/weather/WeatherStream.java
+++ src/main/java/ba/weather/WeatherStream.java
@@ -1,7 +1,6 @@
 package ba.weather;
 
 import org.streampipes.model.impl.EventStream;
-import org.streampipes.model.impl.eventproperty.EventProperty;
 import org.streampipes.model.impl.graph.SepDescription;
 import org.streampipes.model.vocabulary.Geo;
 import org.streampipes.sdk.builder.DataStreamBuilder;
@@ -10,27 +9,35 @@ import org.streampipes.sdk.helpers.Formats;
 import org.streampipes.sdk.helpers.Protocols;
 import org.streampipes.sources.AbstractAlreadyExistingStream;
 
+/**
+ * @author Michael Jahns 
+ * Description of the Stream provided by the RestProducer
+ */
 public class WeatherStream extends AbstractAlreadyExistingStream {
 
-    @Override
-    public EventStream declareModel(SepDescription sepDescription) {
-        return DataStreamBuilder.create("air-weather-stream", "Weather Stream", "A data  that " +
-                "holds Weather and Air Data Produced by stations.")
-                .property(EpProperties.timestampProperty("timestamp"))
-                .property(EpProperties.timestampProperty("getRequestTimestamp"))
-                .property(EpProperties.stringEp("station", "http://schema.org/name"))
-                .property(EpProperties.doubleEp("latitude", Geo.lat))
-                .property(EpProperties.doubleEp("longitude", Geo.lng))
-                .property(EpProperties.doubleEp("heigth", Geo.alt))
-//                http://www.qudt.org/qudt/owl/1.0.0/unit/Instances.html#Lux
-                .property(EpProperties.integerEp("ozn", "http://schema.org/Integer"))
-                .property(EpProperties.integerEp("lux", "http://schema.org/Integer"))
-                .property(EpProperties.integerEp("no2", "http://schema.org/Integer"))
-                .property(EpProperties.integerEp("so2", "http://schema.org/Integer"))
-                .property(EpProperties.integerEp("pm10", "http://schema.org/Integer"))
-                .format(Formats.jsonFormat())
-                .protocol(Protocols.kafka(WeatherConfig.INSTANCE.getKafkaHost(), WeatherConfig.INSTANCE.getKafkaPort(),
-                        "my.weather.lubw"))
-                .build();
-    }
+	@Override
+	public EventStream declareModel(SepDescription sepDescription) {
+		return DataStreamBuilder
+				.create("air-weather-stream", "Weather Stream",
+						"A data  that " + "holds Weather and Air Data Produced by stations.")
+				.property(EpProperties.timestampProperty("timestamp"))
+				.property(EpProperties.timestampProperty("getRequestTimestamp"))
+				.property(EpProperties.stringEp("station", "http://schema.org/name"))
+				.property(EpProperties.doubleEp("latitude", Geo.lat))
+				.property(EpProperties.doubleEp("longitude", Geo.lng))
+				.property(EpProperties.doubleEp("heigth", Geo.alt))
+
+				.property(EpProperties.integerEp("ozn",
+						"https://www.eea.europa.eu/themes/air/air-quality/resources/glossary/g-m3"))
+				.property(EpProperties.integerEp("lux", "http://www.qudt.org/qudt/owl/1.0.0/unit/Instances.html#Lux"))
+				.property(EpProperties.integerEp("no2",
+						"https://www.eea.europa.eu/themes/air/air-quality/resources/glossary/g-m3"))
+				.property(EpProperties.integerEp("so2",
+						"https://www.eea.europa.eu/themes/air/air-quality/resources/glossary/g-m3"))
+				.property(EpProperties.integerEp("pm10",
+						"https://www.eea.europa.eu/themes/air/air-quality/resources/glossary/g-m3"))
+				.format(Formats.jsonFormat()).protocol(Protocols.kafka(WeatherConfig.INSTANCE.getKafkaHost(),
+						WeatherConfig.INSTANCE.getKafkaPort(), "my.weather.lubw"))
+				.build();
+	}
 }
